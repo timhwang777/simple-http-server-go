e5931b83c69fc2d6941223fcfd37419b6a46827e
Tim@Davis-Dorm-WSL2 <timhwang@ucdavis.edu>
2023-11-16 12:36:39 -0800

    stage 3

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -21,10 +22,29 @@ func main() {
 
 	defer conn.Close()
 
-	response := "HTTP/1.1 200 OK\r\n\r\n"
+	readBuf := make([]byte, 1024)
+	_, err = conn.Read(readBuf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		os.Exit(1)
+	}
+
+	lines := strings.Split(string(readBuf), "\r\n")
+	requestLine := strings.Split(lines[0], " ")
+	path := requestLine[1]
+
+	var response string
+	switch path {
+	case "/":
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+		break
+	default:
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
 	_, err = conn.Write([]byte(response))
 	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
+		fmt.Println("Error writing: ", err.Error())
 		os.Exit(1)
 	}
 }
